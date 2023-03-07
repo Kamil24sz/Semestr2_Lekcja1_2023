2d08740afec3e73ac61fd4d47f135bad0efb4e65
Kamil Szczypczyk <kamil.szczypczyk@microsoft.wsei.edu.pl>
2023-03-07 20:38:16 +0100

    Dokończone zadania

--- AplikacjaOkienkowa/MainForm.cs
+++ AplikacjaOkienkowa/MainForm.cs
@@ -19,13 +19,52 @@ namespace AplikacjaOkienkowa
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Wyczysc();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length == 0)
+            {
+                MessageBox.Show("Nie można wysłać wiadomości bez podania adresata",
+                            "Brak adresata",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+            }
+            else
+            {
+                if(textBox1.Text.Length == 0)
+                {
+                    //pobieranie klikniętej wartości tak/nie z messageBox
+                    DialogResult czyWyslac =
+                    MessageBox.Show("Nie podano tytułu wiadomości. Czy na pewno chcesz wysłać maila?",
+                                "Brak tytułu",
+                                MessageBoxButtons.YesNo,
+                                MessageBoxIcon.Warning);
+                 
+                    if(czyWyslac == DialogResult.Yes)
+                    {
+                        Wyslij(textBox2.Text);
+                    }   
+                }
+                Wyslij(textBox2.Text);
+            }
+        }
+
+        private void Wyczysc()
+        {
             textBox1.Text = "";
             textBox2.Text = "";
             richTextBox1.Text = "";
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void Wyslij(String adresat)
         {
+            MessageBox.Show("Wiadomość została wysłana do " + adresat,
+                                "Wysłano",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+            Wyczysc();
         }
     }
 }
